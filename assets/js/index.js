2a169346f46c846d4018ab60e75e26c293ca18cb
Maryanne Anyango <mayannanneanyango98@gmail.com>
2022-06-28 06:47:05 +0300

    files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,16 @@
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+
+
 var input = document.querySelector('.input_text');
 var main = document.querySelector('#name');
 var temp = document.querySelector('.temp');
